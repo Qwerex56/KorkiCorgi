9b2bd34ea9498c751ea05a876aa6e0898cfad274
Adrian Czubaty <adriancz.prywatny@gmail.com>
2024-10-26 11:55:26 +0200

    Added missing nuget packages and setup user secrets for development

--- KorkiCorgi/Program.cs
+++ KorkiCorgi/Program.cs
@@ -1,4 +1,5 @@
 using KorkiCorgi.Components;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else {
+    // builder.Services.AddDbContext<DbContext>(options => {
+    //     options.UseNpgsql(builder.Configuration["ConnectionStrings:NpgsqlConnection"]);
+    // }); // Connection string for a development scenario, for production uses appsettings
 }
 
 app.UseHttpsRedirection();
