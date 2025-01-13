03c38bc1ee9c23ea71c69b45a57bc950162603f8
Leon <Lemon_Spoko@wp.pl>
2025-01-13 13:53:11 +0100

    cała strona bez kalendarza

--- KorkiCorgi/Program.cs
+++ KorkiCorgi/Program.cs
@@ -15,6 +15,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseStaticFiles(new StaticFileOptions
+{
+OnPrepareResponse = ctx =>
+{
+    ctx.Context.Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate";
+}
+});
 } else {
     // builder.Services.AddDbContext<DbContext>(options => {
     //     options.UseNpgsql(builder.Configuration["ConnectionStrings:NpgsqlConnection"]);
