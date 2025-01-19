b93ee94ba946a0cc789e8dfced6583cf19ab0634
Alban <albanstudios@gmail.com>
2025-01-19 20:29:57 +0100

    AnnouncementList and ProfilPanel component pages completed

---
+++ KorkiCorgi/DataTransferObjects/ProfilPanelDto.cs
@@ -0,0 +1,13 @@
+﻿using KorkiCorgi.Models;
+
+namespace KorkiCorgi.DataTransferObjects;
+
+public class ProfilPanelDto {
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public AccountStatistics AccountStatistics { get; set; } = null!;
+    public string Description { get; set; }
+    public int Cost { get; set; }
+    public bool IsOnline { get; set; } = true;
+    public string MeetingPlace { get; set; } = string.Empty;
+}
--- KorkiCorgi/Services/IAccountService.cs
+++ KorkiCorgi/Services/IAccountService.cs
@@ -7,4 +7,5 @@ public interface IAccountService {
     public object RegisterNewAccount(UserDto userDto);
     public bool LoginAccount(UserDto userDto);
     public User? GetUserByEmail(string email);
+    public User? GetUserById(int id);
 }
